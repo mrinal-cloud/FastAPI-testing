04d6458ec7afe7af65b16eb4265fef3b3b6ef088
Mrinal Sarma <64658095+mrinal-cloud@users.noreply.github.com>
2025-12-16 10:05:02 +0530

    updated alembic env

--- alembicMigrations/env.py
+++ alembicMigrations/env.py
@@ -7,13 +7,13 @@ from alembic import context
 
 from alembic import context
 from apps.models import Base
-# from app.config import settings
+from apps.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", 'postgresql://postgres:jackatPostgresql@localhost:5432/flutterflowBackend')
+    "sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
